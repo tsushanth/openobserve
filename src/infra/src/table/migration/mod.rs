9d5c4cdfa6bf8cd0c802620c573fe307ec72b63e
Loakesh Indiran <rustyloaki@gmail.com>
2026-06-24 16:30:22 +0530

    feat(synthetics): add token_type column, synthetics token auth, job API handlers (Batch E)

--- src/infra/src/table/migration/mod.rs
+++ src/infra/src/table/migration/mod.rs
@@ -127,6 +127,7 @@ mod m20260622_000001_add_org_id_to_short_urls;
 mod m20260624_000001_create_synthetics_monitors;
 mod m20260624_000002_create_synthetics_pending_checks;
 mod m20260624_000003_fix_synthetics_pending_checks_dedup;
+mod m20260624_000004_add_token_type_to_org_ingestion_tokens;
 
 pub struct Migrator;
 
@@ -243,6 +244,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20260624_000001_create_synthetics_monitors::Migration),
             Box::new(m20260624_000002_create_synthetics_pending_checks::Migration),
             Box::new(m20260624_000003_fix_synthetics_pending_checks_dedup::Migration),
+            Box::new(m20260624_000004_add_token_type_to_org_ingestion_tokens::Migration),
         ]
     }
 }
